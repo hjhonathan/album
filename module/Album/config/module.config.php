aaa0301f55d13390c0da77389996561c2b334ce9
Herbert Chumpitaz <herbert.chumpitaz@gmail.com>
2014-06-24 21:11:35 -0500

    Creado el modulo Album, controlador Album

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,37 @@
+<?php
+
+// Define controlador
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    
+    // Define rutas
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/album[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action' => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    
+    // Define vistas
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);

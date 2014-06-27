c50fda36224c8a5abb4993891fe02351d5a10c12
Herbert Chumpitaz <herbert.chumpitaz@gmail.com>
2014-06-26 20:38:49 -0500

    Agregando action add album

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,40 +1,62 @@
 <?php
-
 namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Album\Model\Album;          // <-- Add this import
+use Album\Form\AlbumForm;       // <-- Add this import
 
-class AlbumController extends AbstractActionController {
 
+class AlbumController extends AbstractActionController
+{
     // agregamos este atributo
     protected $albumTable;
 
-    public function indexAction() {
+    public function indexAction()
+    {
         return new ViewModel(array(
             'albums' => $this->getAlbumTable()->fetchAll(),
         ));
     }
 
-    public function addAction() {
+ // Add content to this method:
+    public function addAction()
+    {   
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+    
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+    
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
         
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+		}
+        return array('form' => $form);
     }
 
-    public function editAction() {
-        
+    public function editAction()
+    {
     }
 
-    public function deleteAction() {
-        
+    public function deleteAction()
+    {
     }
 
     // Agregamos este método
-    public function getAlbumTable() {
+    public function getAlbumTable()
+    {
         if (!$this->albumTable) {
             $sm = $this->getServiceLocator();
             $this->albumTable = $sm->get('Album\Model\AlbumTable');
         }
         return $this->albumTable;
     }
-
 }
